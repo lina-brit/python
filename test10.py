35698d993efb2eca0679a04e7e0e5d5789fcadd6
Maikelly <paz.aguilar.maikelly@outlook.com>
2023-02-05 20:42:48 -0500

    Add def

--- test10.py
+++ test10.py
@@ -1,2 +1,16 @@
-print("este es el test 10")
 #asd
+
+def nombre():
+    print("caronte el gato")
+    
+nombre()
+
+def nombres(text, text2):
+    print(text, text2)
+nombres("caronte", "gotas")
+    
+
+def letras(letra1, letra2, letra3):
+    print(letra1, letra2, letra3)
+
+letras ("a","b", "c")
