7e95f2318c2a745904a34df3521ed4e7f2084369
Britny Maikelly Paz Aguilar <113409976+lina-brit@users.noreply.github.com>
2023-03-04 19:05:37 -0500

    Agregando funciones de multiplicación y suma
    Se a agregado 2 funciones de multiplicación y suma

---
+++ test13.py
@@ -0,0 +1,14 @@
+
+def suma(x, y):
+    print(x + y)
+
+numero1 = int(input("introduce un numero natural:"))
+numero2 = int(input("introduce otro numero natural:"))
+suma(numero1, numero2)
+
+def multiplicacion(x,y):
+    print(x * y)
+
+numero0 = float(input("Introduce un numero decimal"))
+numero3= float(input("Introduce un numero decimal"))
+multiplicacion(numero0, numero3)
